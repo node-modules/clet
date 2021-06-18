e86557349c760115ef2fcde3c6f744612163b8a5
TZ <atian25@qq.com>
2021-06-19 00:00:47 +0800

    feat: wait event

--- test/fixtures/long-run.js
+++ test/fixtures/long-run.js
@@ -1,41 +1,22 @@
 #!/usr/bin/env node
 
-import { sleep } from '../utils.js';
-import fs from 'fs/promises';
-
-console.log('this is long run process');
-
-const filePath = process.env.filePath;
-
-console.log(`log to ${filePath}`);
-
-async function log(msg) {
-  console.log(msg);
-  await fs.appendFile(filePath, msg);
-}
+import * as utils from '../utils.js';
 
 async function run() {
+  console.log('this is long run process');
+  const filePath = process.env.filePath;
+  console.log('starting egg....');
 
-  log('starting egg....');
-  log('loading egg plugin');
-  log('loading egg controller');
+  await utils.sleep(2000);
+  console.log(`log to ${filePath}`);
+  await utils.writeFile(filePath, 'this is a log');
 
-  await sleep(1000);
-
-  // write file
-  // event
   process.send && process.send({ action: 'egg-ready' });
-  process.send && process.send('egg-ready');
-  log('egg started at localhost:8080');
+  // process.send && process.send('egg-ready');
+  // console.log('egg started at localhost:8080');
 
-  await sleep(200);
-  process.send && process.send('some message');
-
-  for (let i = 0; i < 10; i++) {
-    await sleep(100);
-    console.log(i);
-  }
   console.log('stopping...');
+  await utils.del(filePath);
 }
 
 run().catch(console.error);
--- test/runner.test.js
+++ test/runner.test.js
@@ -1,13 +1,18 @@
 import runner from '../lib/runner';
 import * as utils from './utils';
+import path from 'path';
 
 describe('test/runner.test.js', () => {
   const fixtures = utils.resolve(import.meta, 'fixtures');
+  const tmpDir = utils.getTempDir(expect);
+
+  beforeEach(() => utils.initDir(tmpDir));
 
   it('should work', async () => {
     const instance = await runner()
       .cwd(fixtures)
       .fork('./simple.js', [ '--name=test' ])
+      .wait('close')
       .stdout('this is simple bin')
       .stdout(/argv:/)
       .notStdout('xxxx')
@@ -21,17 +26,18 @@ describe('test/runner.test.js', () => {
   });
 
   it('should await event', async () => {
+    const filePath = path.join(tmpDir, 'event.md');
+
     await runner()
       .cwd(fixtures)
+      .env('filePath', filePath)
+      .notFile(filePath)
       .fork('./long-run.js')
 
-      .wait('stdout', /loading egg controller/)
-      // .log('result.stdout')
-
+      .tap(() => console.log(new Date().toLocaleString()))
+      .notFile(filePath)
       .wait('message', { action: 'egg-ready' })
-      .stdout(/send message to parent/)
-      .notStdout(/after send message/)
-      // .log('result.stdout')
+      .file(filePath)
 
       .wait('close')
       .code(0)
